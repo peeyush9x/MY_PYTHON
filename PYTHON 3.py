d1c3f5e6ec75fde7ef01eb02648e587a2227dc7a
peeyush maurya <peeyushmaurya7666@gmail.com>
2025-07-23 16:29:51 +0530

    Add files via upload

---
+++ PYTHON 3.py
@@ -0,0 +1,186 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:/Users/PC-11/AppData/Local/Programs/Python/Python313/chandrakesh.py
+0
+1
+2
+3
+4
+0
+1
+2
+3
+4
+0
+1
+
+= RESTART: C:/Users/PC-11/AppData/Local/Programs/Python/Python313/chandrakesh.py
+0
+1
+2
+3
+4
+0
+1
+2
+3
+4
+
+= RESTART: C:/Users/PC-11/AppData/Local/Programs/Python/Python313/chandrakesh.py
+0
+1
+2
+3
+4
+0
+1
+2
+3
+4
+
+= RESTART: C:/Users/PC-11/AppData/Local/Programs/Python/Python313/chandrakesh.py
+0
+1
+2
+3
+4
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+= RESTART: C:/Users/PC-11/AppData/Local/Programs/Python/Python313/chandrakesh.py
+Traceback (most recent call last):
+  File "C:/Users/PC-11/AppData/Local/Programs/Python/Python313/chandrakesh.py", line 2, in <module>
+    name  =  int (" enter your name  (type exit to stop) ")
+ValueError: invalid literal for int() with base 10: ' enter your name  (type exit to stop) '
+
+= RESTART: C:/Users/PC-11/AppData/Local/Programs/Python/Python313/chandrakesh.py
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) h
+ enter your name  (type exit to stop) h
+ enter your name  (type exit to stop) h
+ enter your name  (type exit to stop) h
+ enter your name  (type exit to stop) h
+ enter your name  (type exit to stop) h
+ enter your name  (type exit to stop) h
+ enter your name  (type exit to stop) h
+ enter your name  (type exit to stop) h
+ enter your name  (type exit to stop) h
+ enter your name  (type exit to stop) h
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) hh
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) h
+ enter your name  (type exit to stop) h
+ enter your name  (type exit to stop) h
+ enter your name  (type exit to stop) h
+ enter your name  (type exit to stop) hhDDFAW
+ enter your name  (type exit to stop) FW
+ enter your name  (type exit to stop) F
+ enter your name  (type exit to stop) FDGFAS
+ enter your name  (type exit to stop) FGFV
+ enter your name  (type exit to stop) FF
+ enter your name  (type exit to stop) FSD
+ enter your name  (type exit to stop) GGDF
+ enter your name  (type exit to stop) FVDEF
+ enter your name  (type exit to stop) GSDF
+ enter your name  (type exit to stop) GFDFW
+ enter your name  (type exit to stop) EF
+ enter your name  (type exit to stop) GF
+ enter your name  (type exit to stop) GWRE
+ enter your name  (type exit to stop) GEF
+ enter your name  (type exit to stop) GERERgfWRER
+ enter your name  (type exit to stop) ER
+ enter your name  (type exit to stop) RFEWR
+ enter your name  (type exit to stop) GEWR
+ enter your name  (type exit to stop) ER
+ enter your name  (type exit to stop) FR
+ enter your name  (type exit to stop) EWG
+ enter your name  (type exit to stop) We
+ enter your name  (type exit to stop) r'er
+ enter your name  (type exit to stop) rfwe
+ enter your name  (type exit to stop) fw
+ enter your name  (type exit to stop) efdr
+ enter your name  (type exit to stop) gweg
+ enter your name  (type exit to stop) regfer
+ enter your name  (type exit to stop) ge
+ enter your name  (type exit to stop) rgtr
+ enter your name  (type exit to stop) get
+ enter your name  (type exit to stop) get
+ enter your name  (type exit to stop) g
+ enter your name  (type exit to stop) etg
+ enter your name  (type exit to stop) etrgetr
+ enter your name  (type exit to stop) ge
+ enter your name  (type exit to stop) ge
+ enter your name  (type exit to stop) get
+ enter your name  (type exit to stop) gder
+ enter your name  (type exit to stop) hss
+ enter your name  (type exit to stop) g
+ enter your name  (type exit to stop) tr
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) tg
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) g
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) g\
+ enter your name  (type exit to stop) s
+ enter your name  (type exit to stop) ss
+ enter your name  (type exit to stop) s
+ enter your name  (type exit to stop) scv
+ enter your name  (type exit to stop) v
+ enter your name  (type exit to stop) ss
+ enter your name  (type exit to stop) 
+ enter your name  (type exit to stop) 
+= RESTART: C:/Users/PC-11/AppData/Local/Programs/Python/Python313/chandrakesh.py
