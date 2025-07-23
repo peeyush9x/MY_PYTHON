89b06391f9dd134785b4482a9f288e1aefc5fc59
peeyush maurya <peeyushmaurya7666@gmail.com>
2025-07-23 16:26:31 +0530

    Add files via upload

---
+++ Practical 1.1.py
@@ -0,0 +1,14 @@
+a=5
+b=10
+c=15
+if a < b and a < c:
+     print("a is smallest")
+elif b < a and b < c :
+     print("b is smallest")
+else:
+       print("c is smallest")
+
+
+
+for i in range (1,10000000000000) :
+ print(2 * i)
