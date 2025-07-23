15e52c56933f231b4872d243364305176e43366a
peeyush maurya <peeyushmaurya7666@gmail.com>
2025-07-23 16:29:34 +0530

    Add files via upload

---
+++ PYTHON 2.py
@@ -0,0 +1,26 @@
+print("hello world")
+print("Gaurav")
+a=5
+b=13
+print( a+b, "addition")
+
+a=12
+b=7
+print(a-b ,"subtitution")
+
+a=15
+b=17
+print(a*b ,"multiplication")
+
+a=44
+b=2
+print(a%b ,"modules")
+
+a=14
+b=2
+print(a//b ,"floor division")
+
+a=6
+b=7
+print(a**b, "exponential")
+
